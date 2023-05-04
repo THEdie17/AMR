4fc7992abe5ff4953a9176a0c5c461faf45c61ca
THEdie17 <dieguetefernansanz@gmail.com>
2023-05-04 19:20:31 +0200

    begin

--- app/src/main/java/com/example/activemovereminder/MainActivity.kt
+++ app/src/main/java/com/example/activemovereminder/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.activemovereminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var loginbutton: Button
+    private lateinit var registerbutton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginbutton = findViewById(R.id.loginButton)
+        registerbutton = findViewById(R.id.registerButton)
+
+        loginbutton.setOnClickListener {
+            //va a la pantalla Login
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        registerbutton.setOnClickListener {
+            //va a la pantalla Login
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
