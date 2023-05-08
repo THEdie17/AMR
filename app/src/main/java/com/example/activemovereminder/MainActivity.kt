19c84bc96c5dfc2f17fb8ab8668fb3d66f2b2fc8
LauraHNunez <110181542+LauraHNunez@users.noreply.github.com>
2023-05-08 15:54:42 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/activemovereminder/MainActivity.kt
+++ app/src/main/java/com/example/activemovereminder/MainActivity.kt
@@ -6,23 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //Creem els 2 botons necessaris que ens portaran a la següent pàgina
     private lateinit var loginbutton: Button
     private lateinit var registerbutton: Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loginbutton = findViewById(R.id.loginButton)
         registerbutton = findViewById(R.id.registerButton)
 
+        //On end dirigeix quan clickem el botó de Login
         loginbutton.setOnClickListener {
             //va a la pantalla Login
             val intent = Intent(this, Login::class.java)
             startActivity(intent)
-        }
+        }//LoginButton 
+        
+        //On end dirigeix quan clickem el botó de Sign Up
         registerbutton.setOnClickListener {
             //va a la pantalla Login
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
-        }
-    }
-}
+        }//RegisterButton
+        
+    }//fun onCreate()
+}//MainActivity
