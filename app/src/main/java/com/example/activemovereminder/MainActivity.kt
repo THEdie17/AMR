08dafd15870f27b64a4560226f2765f49370e20e
THEdie17 <dieguetefernansanz@gmail.com>
2023-05-09 16:54:26 +0200

    halfregister

--- app/src/main/java/com/example/activemovereminder/MainActivity.kt
+++ app/src/main/java/com/example/activemovereminder/MainActivity.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var loginbutton: Button
     private lateinit var registerbutton: Button
+    private lateinit var auth: FirebaseAuth
+    private var user: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loginbutton = findViewById(R.id.loginButton)
         registerbutton = findViewById(R.id.registerButton)
+
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance("https://junkerultra-default-rtdb.europe-west1.firebasedatabase.app/")
+
+        //assigna valor a user
+        auth = FirebaseAuth.getInstance()
+        user = auth.currentUser
 
         loginbutton.setOnClickListener {
             //va a la pantalla Login
@@ -23,6 +36,18 @@ class MainActivity : AppCompatActivity() {
             //va a la pantalla Login
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
+        }
+    }
+    override fun onStart() {
+        usuariLogejat()
+        super.onStart()
+    }
+
+    private fun usuariLogejat() {
+        if (user != null) {
+            val intent = Intent(this, WeekMenu::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
---
+++ app/src/main/java/com/example/activemovereminder/WeekMenu.kt
@@ -0,0 +1,4 @@
+package com.example.activemovereminder
+
+class WeekMenu {
+}
