799eadd119c721317c21522fe55b022504515004
LauraHNunez <110181542+LauraHNunez@users.noreply.github.com>
2023-05-12 17:18:37 +0200

    Update Login.kt

--- app/src/main/java/com/example/jankenultra/Login.kt
+++ app/src/main/java/com/example/jankenultra/Login.kt
@@ -2,7 +2,6 @@ package com.example.jankenultra
 
 import android.content.Intent
 import android.graphics.Typeface
-import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
